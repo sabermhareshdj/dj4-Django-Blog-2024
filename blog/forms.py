f5e91a45facf6a2ad38604f142948959e1e2d1bb
Saber <dj.saber.mharesh@gmail.com>
2023-12-29 19:51:35 +0300

    add new template

--- blog/forms.py
+++ blog/forms.py
@@ -1,11 +1,18 @@
 from django import forms
 from .models import Post ,Comment
+from django_summernote.widgets import SummernoteWidget, SummernoteInplaceWidget 
+from django_summernote.fields import SummernoteTextFormField, SummernoteTextField
+
+
 
 class PostForm(forms.ModelForm):
+    content = forms.CharField(widget=SummernoteWidget())
     class Meta:
         model = Post
         #fields = '__all__'
         exclude = ('author',)
+        
+
 
 class CommentForm(forms.ModelForm):
     class Meta:
--- project/settings.py
+++ project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'taggit',
     'django_summernote',
+    "django_bootstrap5",
     'blog',
 ]
 
